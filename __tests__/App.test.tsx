5b70ec0004913ca16d051d6690e91469864d2e11
Joel Sebastian <53466652+JoelSebstn@users.noreply.github.com>
2024-03-06 10:10:26 +0530

    Update App.test.tsx

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,22 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import { it } from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { HeaderRow } from '../src/components/headerRow/headerRow';
+import { render } from '@testing-library/react-native';
+describe('sample', () => {
+    it(`tests sample add`, () => {
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+        expect(2 + 1).toBe(3);
+    })
+    it('should render the label of the social link', () => {
+        const { getByText } = render(
+            <HeaderRow />,
+        );
+        const label = getByText("Name");
+        expect(label).toBeTruthy();
+    });
+})
